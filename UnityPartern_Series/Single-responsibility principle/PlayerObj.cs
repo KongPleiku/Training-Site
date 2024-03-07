3a3b93801fe37baf0d44e896c727e833ec05e0ab
KongPleiku <conkubenho27@gmail.com>
2024-03-07 13:14:15 +0700

    Added Unity Parttern Series

---
+++ UnityPartern_Series/Single-responsibility principle/PlayerObj.cs
@@ -0,0 +1,44 @@
+﻿namespace Single_responsibility_principle;
+class PlayerObj
+{
+    Movement movement;
+    PlayerAnimation animation;
+
+    Input input; 
+
+    static void Main(string[] args)
+    {
+        while(true){
+            Console.WriteLine("running.....");
+        }
+    }
+
+    void update(){
+        input.getInput();
+        movement.Move(10f,this);
+        animation.PlayAnimation(this);
+    }
+}
+
+class Movement{
+    public void Move(float speed, PlayerObj p){
+        Console.Write("Moved");
+    }
+}
+
+class PlayerAnimation{
+    public void PlayAnimation(PlayerObj p){
+        Console.Write("Played");
+    }
+}
+
+class Input{
+    public void getInput(){
+        Console.Write("Enter Input: ");
+        string input = Console.ReadLine();
+
+        if(input == "play"){
+            Console.WriteLine("Executed! ");
+        }
+    }
+}
